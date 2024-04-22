b5a7dea88e26532d8bf93e579c90f3edd11dd0e8
AlexTLDR <alex.badragan@protonmail.com>
2024-04-22 14:03:03 +0200

    adding cli features to the calculator

--- investment-calculator/investment_calculator.go
+++ investment-calculator/investment_calculator.go
@@ -6,10 +6,18 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+	const inflationRate = 2.5
+	var investmentAmount, expectedReturnRate, years float64
 
-	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	fmt.Print("Enter investment amount: ")
+	fmt.Scan(&investmentAmount)
+	fmt.Print("Enter expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+	fmt.Print("Enter number of years: ")
+	fmt.Scan(&years)
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, float64(years))
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, float64(years))
 	fmt.Printf("Future value is € %.2f\n", futureValue)
+	fmt.Printf("Future real value is € %.2f\n", futureRealValue)
 }
